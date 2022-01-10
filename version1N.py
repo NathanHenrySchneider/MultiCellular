192e2b75124af1a5951bf1eee79c219f83d22442
Nate Schneider <nate8schneider@gmail.com>
2022-01-09 22:43:45 -0500

    second Day
    more code

--- version1N.py
+++ version1N.py
@@ -6,16 +6,24 @@ import time
 import math
 import random
 import sys
+import threading
+
+# from version1M import initialzationMainMap
 
 # set size to 50x50
 #Hardcoded values
 
 global x_bound
-x_bound = 50
+x_bound = 5
 global y_bound
-y_bound = 50
+y_bound = 5
 global mainMap
 mainMap = None
+global mapsArr
+mapsArr = []
+global numMaps
+numMaps = 5
+# number of maps is equal to the size of the mapsArr
 
 
 
@@ -30,63 +38,64 @@ class twoDimArray:
         self.id = ID
 
     def __str__(self):
-        return("twoDimArray: " + self.id + "/n")
+        isMainVal = " is " if self.isMain else " is not "
+        return("twoDimArray: " + str(self.id) + isMainVal +"the main map\n")
 
 
 
 
-def findNeighborhoodValue(arr, index):
-    x = index % (x_bound + 1)
-    y = (index - x) / (x_bound + 1)
+def findNeighborhoodValue(arr, x, y):
     #get score from each adjacent cell starting at top going clockwise
     score = 0
-    value = arr[x][y]
+    value = arr[y][x]
+
+# arr[y+1][x]
 
     #top
     if (y != 0):
-        top = arr[index - x_bound - 1]
+        top = arr[y-1][x]
         score += top
         #print("top:"+str(top))
 
     #diagonal top right
     if (x != x_bound and y != 0):
-        top_right = arr[index - x_bound]
+        top_right = arr[y - 1][x + 1]
         score += top_right
         #print("top_right:"+str(top_right))
 
     #right
     if (x != x_bound):
-        right = arr[index + 1]
+        right = arr[y][x + 1]
         score += right
         #print("right:"+str(right))
 
     #diagonal bottom right
     if (x != x_bound and y != y_bound):
-        bottom_right = arr[index + x_bound + 2]
+        bottom_right = arr[y + 1][x + 1]
         score += bottom_right
         #print("bottom_right:"+str(bottom_right))
 
     #bottom
     if (y != y_bound):
-        bottom = arr[index + x_bound + 1]
+        bottom = arr[y + 1][x]
         score += bottom
         #print("bottom:"+str(bottom))
 
     #diagonal bottom left
     if (y != y_bound and x != 0):
-        bottom_left = arr[index + x_bound]
+        bottom_left = arr[y + 1][x - 1]
         score += bottom_left
         #print("bottom_left:"+str(bottom_left))
 
     #left
     if (x != 0):
-        left = arr[index - 1]
+        left = arr[y][x - 1]
         score += left
         #print("left:"+str(left))
 
     #diagonal top left
     if (x != 0 and y != 0):
-        top_left = arr[index - x_bound - 2]
+        top_left = arr[y - 1][x - 1]
         score += top_left
         #print("top_left:"+str(top_left))
 
@@ -95,22 +104,66 @@ def findNeighborhoodValue(arr, index):
 
 
 def generateArray():
-    cellMap = [[]]
+    cellMap = []
     y = 0
     while (y < y_bound):
         x = 0
+        newXarr = []
         while (x < x_bound):
-            cellMap[x][y] = random.randint(0,1)
+            newXarr.append(random.randint(0,1))
             x += 1
+        cellMap.append(newXarr)
         y += 1
     return cellMap
 
 
 
-def calculateNewVal (arr, index):
-    retVal = findNeighborhoodValue(arr, index)
-    score = retVal[0]
-    value = retVal[1]
+def initializeMainMap():
+    tempMainObj = twoDimArray(0)
+    tempMainArr = generateArray()
+    tempMainObj.isMain = True
+    tempMainObj.arrMap = tempMainArr
+    global mainMap
+    mainMap = tempMainObj
+
+def initializationSetUp():
+    initThread = threading.Thread(target=initializeMainMap())
+    initThread.start()
+    tempN = 0
+    while (tempN < numMaps):
+        tempMap = twoDimArray(0)
+        tempMap.arrMap = generateArray()
+        tempMap.id = int(tempN)
+        mapsArr.append(tempMap)
+        tempN += 1
+    initThread.join()
+    # printMaps()
+    # print(mapsArr[0].arrMap)
+
+def printMaps():
+    loopN = 0
+    while (loopN < numMaps):
+        print(mapsArr[loopN])
+        loopN += 1
+
+def updateMap(id):
+    oldArr = mapsArr[id.arrMap]
+    newArr = []
+    tempY = 0
+    while (tempY < y_bound):
+        tempX = 0
+        tempNewXarr = []
+        while (tempX < x_bound):
+            tempNewVal = calculateNewVal(findNeighborhoodValue(oldArr, tempX, tempY), oldArr[tempY][tempX])
+            tempNewXarr.append(tempNewVal)
+            tempX += 1
+        newArr.append(tempNewXarr)
+        tempY += 1
+    mapsArr[id.arrMap] = newArr
+
+
+
+def calculateNewVal (score, value):
     if (value == 1 and score < 2):
         return 0
     if (value == 1 and (score == 2 or score == 3)):
@@ -122,14 +175,13 @@ def calculateNewVal (arr, index):
     return 0
 
 
-def initializeMainMap():
-    tempMainObj = twoDimArray()
-    tempMainArr = generateArray()
-    tempMainObj.isMain = True
-    tempMainObj.arrMap = tempMainArr
-    global mainMap
-    mainMap = tempMainObj
+def updateMaps():
+    tempIDN = 0
+    threads = []
+    while (tempIDN < numMaps):
+        containedN = tempIDN
+        threads.append(threading.Thread(target=initializeMainMap(updateMap(containedN))))
+        threads[containedN].start()
+        tempIDN += 1
 
-def initializationSetUp():
-    initializeMainMap()
-    
+# initializationSetUp()
